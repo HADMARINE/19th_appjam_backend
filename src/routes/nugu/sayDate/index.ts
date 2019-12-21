48cdb0bfa5938c8cf6e6d056f3a15c552ebe5031
HADMARINE <joonlee031@live.com>
2019-12-22 07:13:13 +0900

    get product data from db

--- src/routes/nugu/sayDate/index.ts
+++ src/routes/nugu/sayDate/index.ts
@@ -7,6 +7,8 @@ import nugu from '../../../lib/nugu.json';
 import TestUser from '../../../models/TestUser';
 import User from '../../../models/User';
 import Push from '../../../models/Push';
+import Product from '../../../models/Product';
+import getRandomArbitrary from '../../../lib/getRandomArbitrary';
 
 router.use(bodyParser.json());
 
@@ -26,14 +28,30 @@ router.post('/', async (req, res, next) => {
     const today = new Date();
 
     console.log(passedDate);
+
     if (passedDate % 100 >= 94 && passedDate % 100 < 100) {
+      const product: any = await Product.find({ flag: 1 });
+      console.log(product);
+      if (product.length === 0) {
+        return throwError('상품 데이터가 존재하지 않습니다.', 404, {
+          logError: true,
+          data: {}
+        });
+      }
+
+      const randomNumber = getRandomArbitrary(0, product.length);
+
+      const outputProduct = product[randomNumber];
+      console.log(outputProduct, '|||', randomNumber);
       nugu.response.output = {
         date: `오늘은 ${today.getMonth() +
           1}월 ${today.getDate()}일, ${(Math.floor(passedDate / 100) + 1) *
           100}일. ${100 -
           Math.ceil(
             passedDate % 100
-          )}일 전 입니다. 여자친구에게 따뜻한 USB 온열 무릎담요를 선물해보세요. 가격은 28800 원입니다.`
+          )}일 전 입니다. 여자친구에게 줄 선물로 추천하는 상품은 ${
+          outputProduct.name
+        }입니다. 가격은 ${outputProduct.price} 원입니다.`
       };
 
       const push = new Push({ content: '온열담요' });
