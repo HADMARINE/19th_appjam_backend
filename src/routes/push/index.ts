650ebd5bf7a3327a5853b4045637b6a34a6fb2af
HADMARINE <joonlee031@live.com>
2019-12-22 02:06:56 +0900

    push deactivation added

--- src/routes/push/index.ts
+++ src/routes/push/index.ts
@@ -13,6 +13,18 @@ router.get('/', async (req, res, next) => {
     const push = await Push.find({ isServed: true });
     if (!push) return throwError('받아올 데이터가 없습니다.', 404);
 
+    push.map(async data => {
+      // tslint:disable-next-line: await-promise
+      await Push.findOneAndUpdate(
+        { _id: data._id },
+        {
+          $set: {
+            isServed: true
+          }
+        }
+      );
+    });
+
     res.json({ data: push });
   } catch (error) {
     next(error);
