a97bf5429a73024a296076675f0bcd68b57f9d4d
HADMARINE <joonlee031@live.com>
2019-12-21 17:45:17 +0900

    test code

---
+++ src/routes/test/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+
+import bodyParser from 'body-parser';
+import throwError from '../../lib/throwError';
+
+router.use(bodyParser.json());
+
+router.post('/', (req: any, res: any) => {
+  res.json({ test: '접속 성공' });
+});
+
+module.exports = router;
