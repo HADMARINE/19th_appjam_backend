b18bec0cb852bc01ca5ec7a41aa1c99e1e8a65fe
HADMARINE <joonlee031@live.com>
2019-12-22 01:46:24 +0900

    Create index.ts

---
+++ src/routes/nugu/sayDelivery/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const router = express.Router();
+
+import bodyParser from 'body-parser';
+import throwError from '../../../lib/throwError';
+import nugu from '../../../lib/nugu.json';
+
+router.use(bodyParser.json());
+
+router.post('/', (req: any, res: any) => {
+  nugu.response.output = {
+    delivery: '현재 배송중인 상품은 커플링 입니다. 현재 옥천 허브 에 있습니다.'
+  };
+  console.log('post');
+  res.json(nugu.response);
+});
+
+module.exports = router;
