a6a50c0565e22806f97fed1fbd8aed52eab02832
HADMARINE <joonlee031@live.com>
2019-12-22 01:22:12 +0900

    Update index.ts

--- src/routes/nugu/sayDate/index.ts
+++ src/routes/nugu/sayDate/index.ts
@@ -24,7 +24,8 @@ router.post('/', async (req, res, next) => {
 
     const today = new Date();
 
-    if (passedDate % 100 >= 94 && passedDate % 100 <= 0) {
+    console.log(passedDate);
+    if (1 || (passedDate % 100 >= 94 && passedDate % 100 <= 0)) {
       nugu.response.output = {
         date: `오늘은 ${today.getMonth() +
           1}월 ${today.getDate()}일, ${(today.getDate() / 100 + 1) *
