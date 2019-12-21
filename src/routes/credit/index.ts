b6efa455a239e555e16c037ca256d1f280d9615d
HADMARINE <joonlee031@live.com>
2019-12-21 23:15:27 +0900

    created credit endpoint

---
+++ src/routes/credit/index.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+const router = express.Router();
+
+import bodyParser from 'body-parser';
+import throwError from '../../lib/throwError';
+import User, { UserDocument } from '../../models/User';
+import jwt from 'jsonwebtoken';
+
+router.use(bodyParser.json());
+
+router.get('/', (req, res, next) => {
+  try {
+    const token: any = req.headers['x-access-token'];
+    let tokenValue: any;
+    try {
+      tokenValue = jwt.verify(token, process.env.TOKEN_KEY || 'tokenkey');
+    } catch (error) {
+      return throwError('토큰 검증에 실패했습니다.', 403);
+    }
+
+    const credit = User.findOne({ uid: tokenValue.userId }).select('credit');
+
+    res.json({ credit });
+
+    if (!credit) {
+      return throwError('유저 정보가 없습니다.', 403);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', (req, res, next) => {
+  try {
+    const token: any = req.headers['x-access-token'];
+    const { credit } = req.body;
+
+    let tokenValue: any;
+    try {
+      tokenValue = jwt.verify(token, process.env.TOKEN_KEY || 'tokenkey');
+    } catch (error) {
+      return throwError('토큰 검증에 실패했습니다.', 403);
+    }
+
+    User.findOneAndUpdate(
+      { uid: tokenValue.userId },
+      {
+        $set: {
+          credit
+        }
+      }
+    );
+
+    res.status(201).json({ state: true });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
