a5c6ac8b6deeeb4212e59dbeddef7afbb23e9486
HADMARINE <joonlee031@live.com>
2019-12-21 19:14:30 +0900

    test

--- src/routes/test/index.ts
+++ src/routes/test/index.ts
@@ -7,21 +7,21 @@ import nugu from '../../lib/nugu.json';
 
 router.use(bodyParser.json());
 
-router.post('/', (req: any, res: any) => {
-  nugu.response.output = { test: '접속 성공' };
-  console.log('post');
-  res.json(nugu.response);
-});
+// router.post('/', (req: any, res: any) => {
+//   nugu.response.output = { test: '접속 성공' };
+//   console.log('post');
+//   res.json(nugu.response);
+// });
 
-router.get('/', (req, res) => {
-  nugu.response.output = { test: '접속 성공' };
-  console.log('get');
-  res.json(nugu.response);
-});
+// router.get('/', (req, res) => {
+//   nugu.response.output = { test: '접속 성공' };
+//   console.log('get');
+//   res.json(nugu.response);
+// });
 
 router.use('/', (req, res) => {
   nugu.response.output = { test: '접속 성공' };
-  console.log('get');
+  console.log('use');
   return res.json(nugu.response);
 });
 
