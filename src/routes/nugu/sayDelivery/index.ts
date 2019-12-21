05d172a55651c8826d630593e422a8b2247ca69a
HADMARINE <joonlee031@live.com>
2019-12-22 08:34:46 +0900

    get random items

--- src/routes/nugu/sayDelivery/index.ts
+++ src/routes/nugu/sayDelivery/index.ts
@@ -4,14 +4,33 @@ const router = express.Router();
 import bodyParser from 'body-parser';
 import throwError from '../../../lib/throwError';
 import nugu from '../../../lib/nugu.json';
+import Product from '../../../models/Product';
+import getRandomArbitrary from '../../../lib/getRandomArbitrary';
 
 router.use(bodyParser.json());
 
-router.post('/', (req: any, res: any) => {
-  nugu.response.output = {
-    delivery: '현재 배송중인 상품은 커플링 입니다. 현재 옥천 허브 에 있습니다.'
-  };
-  res.json(nugu.response);
+router.post('/', async (req, res, next) => {
+  try {
+    const product: any = await Product.find({ flag: 1 });
+    console.log(product);
+    if (product.length === 0) {
+      return throwError('상품 데이터가 존재하지 않습니다.', 404, {
+        logError: true,
+        data: {}
+      });
+    }
+
+    const randomNumber = getRandomArbitrary(0, product.length);
+
+    const outputProduct = product[randomNumber];
+    console.log(outputProduct, '|||', randomNumber);
+    nugu.response.output = {
+      delivery: `현재 배송중인 상품은 ${outputProduct.name} 입니다. 현재 옥천 허브 에 있습니다.`
+    };
+    res.json(nugu.response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
